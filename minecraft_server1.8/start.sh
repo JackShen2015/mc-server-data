3aa334b033aa635c43f378b749a37fb22be68eaa
Jack <jackshen2011@gmail.com>
2017-08-28 12:38:40 +0800

    2017-8-28

--- minecraft_server1.8/start.sh
+++ minecraft_server1.8/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-java -Xmx768M -Xms512M -jar /home/jack/minecraft_server1.8/minecraft_server.1.8.1.jar
+java -Xmx768M -Xms512M -jar /home/jack/mc-server-data/minecraft_server1.8/minecraft_server.1.8.1.jar
